75885cfdb9435bde9efe5f70ffe748b5a6a51cd5
Manu Bhardwaj <manu@bhardwaj.info>
2018-04-07 22:17:26 -0400

    Upgrade to xcode 9.3

--- Formula Countdown Today/TodayViewController.swift
+++ Formula Countdown Today/TodayViewController.swift
@@ -17,14 +17,14 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         super.viewDidLoad()
         // Do any additional setup after loading the view from its nib.
         
-        let epochOneValue: Double = 105.0/0.4536
-        let epochTwoValue: Double = 78.9/0.4536
+        let epochOneValue: Double = 234
+        let epochTwoValue: Double = 179.9
         
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
         
-        let epochOneDate: Date = formatter.date(from:"2018-02-01")!
-        let epochTwoDate: Date = formatter.date(from:"2018-11-04")!
+        let epochOneDate: Date = formatter.date(from:"2018-04-01")!
+        let epochTwoDate: Date = formatter.date(from:"2018-10-31")!
         
         
         let currentDate: Date = Date()
